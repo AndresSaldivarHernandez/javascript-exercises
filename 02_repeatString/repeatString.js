9076cd8e8ef17cf733c70a6378983635d06d776e
AndresSaldivarHernandez <andres.saldivar57@gmail.com>
2024-10-14 17:40:51 -0700

    Implement solution for second exercise that passes all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        let repeatedString = "";
+        for (let counter=0; counter < num; counter++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
