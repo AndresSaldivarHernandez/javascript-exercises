365ccff6a2e87e045c0e016693be82b29f9185b7
AndresSaldivarHernandez <andres.saldivar57@gmail.com>
2024-10-14 18:26:25 -0700

    Implement solution for third exercise that passes all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = Array.from(string);
+    let arrayLength = stringArray.length;
+    let reversedArray = [];
+    for (counter = arrayLength; counter >= 0; counter--) {
+        reversedArray.push(stringArray[counter]);
+    }
+    let reversedString = reversedArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
