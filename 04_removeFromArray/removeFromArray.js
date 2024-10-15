9afdcd0ecaaca41e2ba6a522f466d613c9fc91c7
AndresSaldivarHernandez <andres.saldivar57@gmail.com>
2024-10-14 22:22:47 -0700

    Implement solution for fourth exercise that passes all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...parameters) {
+    let originalArray = array;
+    let itemsToBeRemoved = parameters;
+    let originalArrayLength = array.length;
+    let numberOfItems = parameters.length;
+    for (let itemsCounter = 0; itemsCounter < numberOfItems; itemsCounter++) {
+        for(let arrayCounter = 0; arrayCounter < originalArrayLength; arrayCounter++){
+            if(itemsToBeRemoved[itemsCounter] === originalArray[arrayCounter]){
+                originalArray.splice(arrayCounter, 1);
+                arrayCounter--;
+            }
+        }
+    }
+    return originalArray;
 };
 
 // Do not edit below this line
