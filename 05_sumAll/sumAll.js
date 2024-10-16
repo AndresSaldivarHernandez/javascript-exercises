66955e36d6d74eeca68368611158405b7495e87d
AndresSaldivarHernandez <andres.saldivar57@gmail.com>
2024-10-15 20:36:08 -0700

    Implement solution for fifth exercise that passes all tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let isValidInput = (Number.isInteger(firstNum) && Number.isInteger(secondNum));
+    let isPositive = (firstNum > 0 && secondNum > 0);
+    if (isValidInput && isPositive){
+        let largerNum = 0;
+        let smallerNum = 0;
+        if (secondNum > firstNum) {
+            largerNum = secondNum;
+            smallerNum = firstNum;
+        } else {
+            smallerNum = secondNum;
+            largerNum = firstNum;
+        }
+        
+        //Formula for sum between two integers, inclusive
+        let finalNum = ((largerNum + smallerNum)*(largerNum-smallerNum+1))/2;
+        return finalNum;
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
