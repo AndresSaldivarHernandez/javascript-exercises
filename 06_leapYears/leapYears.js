3707b57f34fe2ad006e6e7f169f12fdd6ab0eb55
AndresSaldivarHernandez <andres.saldivar57@gmail.com>
2024-10-15 21:01:27 -0700

    Implement solution for sixth exercise that passes all tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let isDivisibleBy4 = (year % 4 == 0);
+    let isDivisibleBy100 = (year % 100 == 0 );
+    let isDivisibleBy400 = (year % 400 == 0);
+    let isLeapYear = true;
 
+    if(isDivisibleBy4 && !isDivisibleBy100){
+        isLeapYear = true;
+        return isLeapYear;
+    } else {
+        if(isDivisibleBy100 && isDivisibleBy400) {
+            isLeapYear = true
+        } else {
+            isLeapYear = false;
+        }
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
