3b9b67ccc09de9296f6686be93c11a9fce62c95b
AndresSaldivarHernandez <andres.saldivar57@gmail.com>
2024-10-16 22:01:43 -0700

    Implement solution for seventh exercises that passes all tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  //formula that converts fahrenheit to celsius
+  let fahrenheitToCelsius = ((tempInFahrenheit - 32)*5)/9;
+  fahrenheitToCelsius = Number(fahrenheitToCelsius.toFixed(1));
+  return fahrenheitToCelsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  //formula that converts celsius to fahrenheit
+  let celsiusToFahrenheit = ((tempInCelsius*9)/5)+32;
+  celsiusToFahrenheit = Number(celsiusToFahrenheit.toFixed(1));
+  return celsiusToFahrenheit;
 };
 
 // Do not edit below this line
